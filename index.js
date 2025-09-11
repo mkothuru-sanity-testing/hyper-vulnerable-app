86fa569ce7369fa8e9ee11bccf9198d379bfd1f1
Karthik Bharadwaj <kbharadwaj@veracode.com>
2025-09-11 20:50:29 +0530

    Added lock json file

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-console.log("Hello from hyper-vulnerable test app!");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Example of using some installed packages to prevent npm prune from removing them
-const _ = require('lodash');
-const moment = require('moment');
-const bluebird = require('bluebird');
+app.get('/', (req, res) => {
+  res.send('Welcome to the Hyper Vulnerable App!');
+});
 
-console.log("Lodash version:", _.VERSION);
-console.log("Current time (moment):", moment().format());
-console.log("Bluebird is ready:", typeof bluebird.Promise === 'function');
-
-// Minimal function
-function add(a, b) {
-  return a + b;
-}
-
-console.log("1 + 2 =", add(1, 2));
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
